37056bcd9e89a2531da509f12050b232d49c7fe1
pedrotchang <pedrochang91@gmail.com>
2022-07-28 16:27:44 -0700

    skills5.js

---
+++ javascript/skills5.js
@@ -0,0 +1,13 @@
+//1
+//var array1 = ["word", "number", "jack"];
+//array1.push("another");
+//array1.push("word");
+//console.log(array1)
+
+//9
+var numbers = [1, 2, 3, 4, 5, 6]
+var index = 0
+while (index < numbers.length) {
+  console.log(numbers[index]);
+  index++
+}
