91e77172870c049ffc52a63854f3dac2ee2c3370
pedrotchang <pedrochang91@gmail.com>
2022-07-28 16:51:28 -0700

    skills6.js

--- javascript/skills6.js
+++ javascript/skills6.js
@@ -1,11 +1,25 @@
-// 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+//// 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+//var people = [
+//  { firstName: "Pedro", lastName: "Chang" },
+//  { firstName: "Hanna", lastName: "Chang" },
+//  { firstName: "Helen", lastName: "Chang" },
+//  { firstName: "Thomas", lastName: "Chang" },
+//  { firstName: "Clara", lastName: "Chang" },
+//]
+//console.log(people[0]["firstName"]);
+//console.log(people[0]["lastName"]);
 
-var people = [
-  { firstName: "Pedro", lastName: "Chang" },
-  { firstName: "Hanna", lastName: "Chang" },
-  { firstName: "Helen", lastName: "Chang" },
-  { firstName: "Thomas", lastName: "Chang" },
-  { firstName: "Clara", lastName: "Chang" },
-]
-console.log(people[0]["firstName"]);
-console.log(people[0]["lastName"]);
+// 6. Make a hash to store 3 different states and their capitals.
+// Then add a new state and capital and print the hash to see the result.
+var capitals = { California: "Sacramento", "New York": "Albany", Illinois: "Springfield" }
+capitals["Ohio"] = ["Columbus"]
+console.log(capitals)
+
+
+
+
+
+
+
+
+
