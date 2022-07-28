7b895f999ce3fbedc9feb8714e438ff705cd07a6
pedrotchang <pedrochang91@gmail.com>
2022-07-28 16:37:09 -0700

    skills6.js

---
+++ javascript/skills6.js
@@ -0,0 +1,11 @@
+// 2. Make an array of hashes to store the first name and last name for 3 different people. Then print out the first person's info.
+
+var people = [
+  { firstName: "Pedro", lastName: "Chang" },
+  { firstName: "Hanna", lastName: "Chang" },
+  { firstName: "Helen", lastName: "Chang" },
+  { firstName: "Thomas", lastName: "Chang" },
+  { firstName: "Clara", lastName: "Chang" },
+]
+console.log(people[0]["firstName"]);
+console.log(people[0]["lastName"]);
